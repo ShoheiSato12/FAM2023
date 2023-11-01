28599958b604ee3a7ae015c158a5280aaf75d895
Shohei Sato <shoheisato12@gmail.com>
2023-11-01 09:46:24 -0300

    new logs

---
+++ Project_2/own/run2_g16.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Loop from 1 to 30
+for i in {22..30}; do
+    filename="./com/$i.com"  # Generate the file name
+    logpath="./log/$i.log"
+    command="g16 $filename $logpath"  # Construct the command
+    echo "Executing: $command"
+    $command
+done
+
+# End of the script
+echo "All commands executed"
